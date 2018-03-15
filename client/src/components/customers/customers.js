0dc529a8be00c29182d985928abbd1336d3cb133
Jennifer Holyer <jenniferholyer@Tims-MacBook-Pro-2.local>
2018-03-15 01:07:33 -0600

    added another .gitignore file. Installed and used style-components

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 import './customers.css';
 
+const UnorderedList = styled.ul`
+  list-style: none;
+  padding: 0;
+  width: 30%;
+  margin: auto;
+`;
+
+const ListItem = styled.li`
+  font-size: 1.5rem;
+  line-height: 2rem;
+  border-bottom: 1px dotted #777;
+`;
 class Customers extends Component {
   constructor() {
     super();
@@ -13,12 +26,17 @@ class Customers extends Component {
 
   componentWillMount() {
     axios.get('/api/customers')
-	.then(customers => this.setState({customers: customers.data}, () => console.log('fetch', customers.data, this.state)))
+	  .then(customers => this.setState({customers: customers.data}, () => console.log('fetch', customers.data, this.state)))
   }
   render() {
     return (
       <div>
         <h2>Customers</h2>
+        <UnorderedList>
+          {this.state.customers.map(customer =>
+            <ListItem key={customer.id} >{customer.firstName} {customer.lastName}</ListItem>
+          )}
+        </UnorderedList>
       </div>
     );
   }
